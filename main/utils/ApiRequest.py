7a305cebd3969f14d66c1b8b527b67a7ae25dae8
Jack Hermanson <jack.hermanson@live.com>
2021-10-21 10:00:41 -0600

    finish up sandbox

--- main/utils/ApiRequest.py
+++ main/utils/ApiRequest.py
@@ -8,7 +8,7 @@ class ApiRequest:
     def __init__(self, url: str):
         self.url = url
 
-    def make_request(self) -> dict:
+    def make_request(self) -> dict or list:
         response = requests.get(self.url)
         raw_data = response.text
         parsed_data = json.loads(raw_data)
