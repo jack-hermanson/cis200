b0f626c603575f59b41a9d909781840c64b98eeb
ashleyslater2 <ashleyslater20@gmail.com>
2021-11-01 16:13:30 -0600

    update routes.py

--- main/modules/advisory/routes.py
+++ main/modules/advisory/routes.py
@@ -7,3 +7,7 @@ advisory = Blueprint("advisory", __name__, url_prefix="/advisory")
 @advisory.route("/")
 def index():
     return render_template("advisory/index.html")
+
+@advisory.route("/")
+def marketing():
+    return render_template("advisory/martketing.html")
