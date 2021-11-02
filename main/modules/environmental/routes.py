a0b1213e7fecd320c89701c43815d2b5667e8f08
Oleath <70612997+Oleath@users.noreply.github.com>
2021-11-01 18:14:40 -0600

    create climate page

--- main/modules/environmental/routes.py
+++ main/modules/environmental/routes.py
@@ -7,3 +7,8 @@ environmental = Blueprint("environmental", __name__, url_prefix="/environmental"
 @environmental.route("/")
 def index():
     return render_template("environmental/index.html")
+
+
+@environmental.route("/climate")
+def climate():
+    return render_template("environmental/climate.html")
