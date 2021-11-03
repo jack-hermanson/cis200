3483b24effa53265e107dbafcd8c05a88e4e05b9
AvaSchneider <ava.schneider@colostate.edu>
2021-11-03 13:39:55 -0600

    Add Health Foods Page

--- main/modules/social/routes.py
+++ main/modules/social/routes.py
@@ -7,3 +7,7 @@ social = Blueprint("social", __name__, url_prefix="/social")
 @social.route("/")
 def index():
     return render_template("social/index.html")
+
+@social.route("/health-foods")
+def health_foods():
+    return render_template("social/health-foods.html")
