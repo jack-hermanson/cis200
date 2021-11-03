2a3784b005631b3d3ce26f450f08d8bec245dacc
kenzielh <mackenziehatchett13@gmail.com>
2021-11-03 15:55:06 -0600

    redo add livestock-sustainability.html

--- main/modules/environmental/routes.py
+++ main/modules/environmental/routes.py
@@ -3,8 +3,12 @@ from flask import Blueprint, render_template
 
 environmental = Blueprint("environmental", __name__, url_prefix="/environmental")
 
-
 @environmental.route("/")
 def index():
     return render_template("environmental/index.html")
+
+@environmental.route("/livestock-sustainability")
+def livestock_sustainability():
+    return render_template("environmental/livestock-sustainability.html")
+
 
