480eb26a340a4a3c44772294b4039d0faf26ce7b
andrewpatton7 <drewpatt@colostate.edu>
2021-11-03 13:26:15 -0600

    Add HTML, Route, Index for Scamming

--- main/modules/social/routes.py
+++ main/modules/social/routes.py
@@ -7,3 +7,7 @@ social = Blueprint("social", __name__, url_prefix="/social")
 @social.route("/")
 def index():
     return render_template("social/index.html")
+
+@social.route("/scamming")
+def scamming():
+    return render_template("social/scamming.html") 
