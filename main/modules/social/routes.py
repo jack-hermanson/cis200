1971f8a462dc1a58a1c31a292b74dfe4f19b74cd
Joe <thatgeekjoe@gmail.com>
2021-11-03 14:12:09 -0600

    Creating Worksite Wellness Page and Index

--- main/modules/social/routes.py
+++ main/modules/social/routes.py
@@ -7,3 +7,7 @@ social = Blueprint("social", __name__, url_prefix="/social")
 @social.route("/")
 def index():
     return render_template("social/index.html")
+
+@social.route("/worksite-wellness")
+def worksite_wellness():
+    return render_template("social/worksite-wellness.html")
