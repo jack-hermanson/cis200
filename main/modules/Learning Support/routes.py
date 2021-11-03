91ead9460883410ef79afb88da6db615acdb94f8
ethanfantlcolo <92336250+ethanfantlcolo@users.noreply.github.com>
2021-11-03 15:36:14 -0600

    finished creating route
    boom bam

--- main/modules/Learning Support/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-learningsupport = Blueprint("learningsupport", __name__, url_prefix="/learningsupport")
-
-
-@learningsupport.route("/")
-def index():
-    return render_template("learningsupport/index.html")
--- main/modules/advisory/routes.py
+++ main/modules/advisory/routes.py
@@ -7,3 +7,7 @@ advisory = Blueprint("advisory", __name__, url_prefix="/advisory")
 @advisory.route("/")
 def index():
     return render_template("advisory/index.html")
+
+@advisory.route("/learning-support")
+def learning_support():
+    return render_template("advisory/learning-support.html")
