a16e287d577658203433b48e3ca8c73571e85894
Jack Hermanson <jack.hermanson@live.com>
2021-11-02 18:18:26 -0600

    fix brewing company template

--- main/modules/sandbox/routes.py
+++ main/modules/sandbox/routes.py
@@ -140,6 +140,7 @@ def bored2():
 @sandbox.route("/Brewing Company")
 def brewing_company():
     brewing_company_data = ApiRequest("https://api.openbrewerydb.org/breweries").make_request()
-    return render_template("sandbox/Brewing Company.html", 
-    brewing_company_data = brewing_company_data
+    return render_template(
+        "sandbox/brewing-company.html",
+        brewing_company_data=brewing_company_data
     )
