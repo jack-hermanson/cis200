3e7ee78815661a69afd3f7bfbedcb17541b6b340
litammy <tammyli@colostate.edu>
2021-11-12 15:16:08 -0700

    added cards

--- main/__init__.py
+++ main/__init__.py
@@ -43,6 +43,7 @@ def create_app(config_class=Config):
                 "https://images.dog.ceo",
                 "https://imgs.xkcd.com/",
                 "https://cdn2.thecatapi.com",
+                "https://provost.colostate.edu"
             ],
             "frame-src": [
                 "https://www.youtube.com/"
