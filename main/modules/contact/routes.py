396a03efd46de4ccc872d559ff9e05c021c1ebb6
Jack Hermanson <jack.hermanson@live.com>
2021-11-21 16:16:18 -0700

    get contact form data

--- main/modules/contact/routes.py
+++ main/modules/contact/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, jsonify, request, url_for
+from time import sleep
 
 contact = Blueprint("contact", __name__, url_prefix="/contact")
 
@@ -8,3 +8,32 @@ contact = Blueprint("contact", __name__, url_prefix="/contact")
 def index():
     return render_template("contact/index.html")
 
+
+@contact.route("/", methods=["POST"])
+def submit_contact():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message")
+    number = request.form.get("number")
+
+    failed_test = False
+    try:
+        parsed_number = int(number)
+        if parsed_number >= 0:
+            failed_test = True
+    except ValueError:
+        failed_test = True
+
+    if failed_test:
+        contact_route = url_for('contact.index')
+        sleep(1)
+        return f"""
+        <p>You failed the bot test. If you're a human, please click the back button or click <a href="{contact_route}">here</a> to try again.</p>
+        <p>Make sure you enter a <b>negative number</b>.</p>
+        """
+
+    return jsonify({
+        "name": name,
+        "email": email,
+        "message": message
+    })
