5e50e1c383f4a403190cd343e88cd82ece7e19cb
Jack Hermanson <jack.hermanson@live.com>
2021-11-21 13:12:22 -0700

    add mission and vision page

--- main/modules/about/routes.py
+++ main/modules/about/routes.py
@@ -7,3 +7,8 @@ about = Blueprint("about", __name__, url_prefix="/about")
 @about.route("/")
 def index():
     return render_template("about/index.html")
+
+
+@about.route("/mission-and-vision")
+def mission_and_vision():
+    return render_template("about/mission-and-vision.html")
