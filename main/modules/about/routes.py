c7d90b9fd8682008fff855587dafeafa0f8935e8
ashleyslater2 <ashleyslater20@gmail.com>
2021-12-01 16:05:56 -0700

    Merge branch 'main' of https://github.com/jack-hermanson/cis200

--- main/modules/about/routes.py
+++ main/modules/about/routes.py
@@ -17,3 +17,15 @@ def mission_and_vision():
 @about.route("/projects")
 def projects():
     return render_template("about/projects.html")
+
+@about.route("/homelessness-in-colorado")
+def homelessness_in_colorado():
+    return render_template("about/homelessness-in-colorado.html")
+
+@about.route("/social-isolation")
+def social_isolation():
+    return render_template("about/social-isolation.html")
+
+@about.route("/sustainable-restaurants")
+def restaurant_sustainability():
+    return render_template("about/sustainable-restaurants.html")
